d756ab0f428891486ee3772466044ac9411a3768
Soe Paing <sopykt09@gmail.com>
2020-09-22 13:10:49 +0630

    prevent unwanted access to plugin

--- wp-content/plugins/soepaing-plugin/soepaing-plugin.php
+++ wp-content/plugins/soepaing-plugin/soepaing-plugin.php
@@ -20,3 +20,32 @@
  * License:           GPL v2 or later
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  */
+
+/*
+Copyright (C) 2020  Dr Soe Paing
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+if ( ! defined('ABSPATH') ) {
+  die;
+}
+
+defined('ABSPATH') or die( 'Hey, You can\t access this file, You silly Human!' );
+
+if ( ! function_exists( 'add_action' ) ) {
+  echo 'Hey, You can\t access this file, You silly Human!';
+  exit;
+}
